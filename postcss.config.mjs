a894ff939e16382c169d77a8063a9248ef6b357d
Phes <nguyenhongphuc41003@gmail.com>
2025-03-23 00:15:13 +0700

    Config material tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,13 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };

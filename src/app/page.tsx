3003e261325f62f9f0f7d78f23c62187aa318e7a
Phes <nguyenhongphuc41003@gmail.com>
2025-04-15 00:00:57 +0700

    Add cards row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import CardsRow from "@/components/home/cards-row";
 import TopSlider from "@/components/home/top-slider";
-import Image from "next/image";
 
 export default function Home() {
   return (
-     <TopSlider />
+    <main>
+      <TopSlider />
+      <div className="px-5 pt-10 pb-50 space-y-10">
+        <CardsRow title="Phim chiếu rạp mới" type_list="phim-le" page={1} sort_field="_id" sort_type="asc" year={2025} limit={15} sort_lang={""} category={""} country={""}/>
+        <CardsRow title="Phim truyền hình mới" type_list="phim-bo" page={1} sort_field="_id" sort_type="asc" year={2025} limit={15} sort_lang={""} category={""} country={""}/>
+        <CardsRow title="Phim hoạt hình mới" type_list="hoat-hinh" page={1} sort_field="_id" sort_type="asc" year={2025} limit={15} sort_lang={""} category={""} country={""}/>
+        <CardsRow title="Gameshow truyền hình thực tế" type_list="tv-shows" page={1} sort_field="_id" sort_type="asc" year={2025} limit={15} sort_lang={""} category={""} country={""}/>
+      </div>
+    </main>
   );
 }
--- src/shared/interfaces/IApiResponse.ts
+++ src/shared/interfaces/IApiResponse.ts
@@ -2,7 +2,12 @@ import { Movie } from "@/shared/interfaces/IMovie";
 import { MovieDetail } from "@/shared/interfaces/IMovieDetail";
 
 export interface ApiResponse {
-    status: boolean;
+    status: boolean | string;
     items: Movie[];
     movie: MovieDetail;
+    data: Items;
+}
+
+interface Items {
+    items: MovieDetail[];
 }

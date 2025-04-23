ff716b205d883be3ab8b76c4335eeafd4be3dbcd
Phes <nguyenhongphuc41003@gmail.com>
2025-04-24 00:43:53 +0700

    Add search page and update search function

---
+++ src/app/(main)/tim-kiem/page.tsx
@@ -0,0 +1,215 @@
+// @ts-nocheck
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from 'next/navigation';
+import Image from "next/image";
+import handleAPIs from "@/lib/api/handleAPI";
+import { ApiResponse, Items } from "@/shared/interfaces/IApiResponse";
+import Link from "next/link";
+import { ArrowLeftIcon, ArrowRightIcon, MagnifyingGlassCircleIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Loading from "@/components/ui/loading";
+import { escapeHtmlAndEncodeSpaces } from "@/lib/utils";
+
+export default function TimKiem() {
+    const [data, setData] = useState<Items>();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [inputPage, setInputPage] = useState('1');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const searchParams = useSearchParams();
+    const query = String(searchParams.get('query')) || "";
+
+    const fetchData = async (pageNumber: number) => {
+        setIsLoading(true);
+        try {
+            const response: ApiResponse = await handleAPIs.getData(
+                `https://phimapi.com/v1/api/tim-kiem?keyword=${escapeHtmlAndEncodeSpaces(query)}&page=${pageNumber}&sort_field=name&sort_type=desc&limit=30`
+            );
+            if (response.status) {
+                setData(response.data);
+                setCurrentPage(pageNumber);
+                setInputPage(pageNumber.toString());
+            } else {
+                console.error('No data found');
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData(1);
+    }, [query]);
+
+    const handlePageChange = (newPage: number) => {
+        if (newPage < 1 || newPage > (data?.params.pagination.totalPages || 1)) return;
+        fetchData(newPage);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.replace(/[^0-9]/g, '');
+        setInputPage(value);
+    };
+
+    const handleInputSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            const pageNumber = parseInt(inputPage);
+            if (pageNumber && pageNumber !== currentPage) {
+                handlePageChange(pageNumber);
+            }
+        }
+    };
+
+    return (
+        <div className="flex flex-col px-5 md:py-29 py-12">
+
+            {isLoading && (
+                <div className="flex justify-center items-center h-full">
+                    <Loading width={70} height={70} className={""} />
+                </div>
+            )}
+
+            {!isLoading && (
+                <>
+                    <div className="flex flex-row gap-3 mb-12">
+                        <MagnifyingGlassCircleIcon className="w-10 h-10" />
+                        <p className="text-[25px] font-medium">{data?.titlePage}</p>
+                    </div>
+
+                    <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                        {data?.items.map((item, index) => (
+                            <div key={index} className="flex flex-col items-center gap-3">
+                                <Link href={`/phim/${item.slug}`}
+                                    className="relative flex-shrink-0 hover:opacity-80 xl:w-[200px] xl:h-[300px] lg:w-[180px] lg:h-[270px] md:w-[175px] md:h-[263px] sm:w-[150px] sm:h-[230px] w-[155px] h-[233px]">
+                                    <Image
+                                        src={`https://phimimg.com/${item.poster_url}`}
+                                        alt={item.name}
+                                        loading="lazy"
+                                        fill
+                                        sizes="(max-width: 205px) 100vw"
+                                        className="rounded-lg object-cover">
+                                    </Image>
+
+                                    <div className="absolute flex flex-row items-center bottom-0 left-1/2 transform -translate-x-1/2 text-[9px] sm:text-[11px]">
+                                        {item.lang === 'Vietsub' && (
+                                            <>
+                                                <p className="border-0 rounded-t-sm bg-[#5e6070] px-2 py-1">
+                                                    P.Đề
+                                                </p>
+                                            </>
+                                        )}
+
+                                        {item.lang === 'Lồng tiếng' && (
+                                            <>
+                                                <p className="border-0 rounded-t-sm bg-[#2ca35d] px-2 py-1">
+                                                    L.Tiếng
+                                                </p>
+                                            </>
+                                        )}
+
+                                        {item.lang === 'Thuyết minh' && (
+                                            <>
+                                                <p className="border-0 rounded-t-sm bg-[#1667cf] px-2 py-1">
+                                                    T.Minh
+                                                </p>
+                                            </>
+                                        )}
+
+
+                                        {item.lang === 'Vietsub + Lồng Tiếng' && (
+                                            <>
+                                                <p className="border-0 rounded-tl-sm bg-[#5e6070] px-2 py-1">
+                                                    P.Đề
+                                                </p>
+                                                <p className="border-0 rounded-tr-sm bg-[#2ca35d] px-2 py-1">
+                                                    L.Tiếng
+                                                </p>
+                                            </>
+                                        )}
+
+                                        {item.lang === 'Vietsub + Thuyết Minh' && (
+                                            <>
+                                                <p className="border-0 rounded-tl-sm bg-[#5e6070] px-2 py-1">
+                                                    P.Đề
+                                                </p>
+                                                <p className="border-0 rounded-tr-sm bg-[#1667cf] px-2 py-1">
+                                                    T.Minh
+                                                </p>
+                                            </>
+                                        )}
+
+                                        {item.lang === 'Vietsub + Thuyết Minh + Lồng Tiếng' && (
+                                            <>
+                                                <p className="border-0 rounded-tl-sm bg-[#5e6070] px-2 py-1">
+                                                    P.Đề
+                                                </p>
+                                                <p className="border-0 bg-[#2ca35d] px-2 py-1">
+                                                    L.Tiếng
+                                                </p>
+                                                <p className="border-0 rounded-tr-sm bg-[#1667cf] px-2 py-1">
+                                                    T.Minh
+                                                </p>
+                                            </>
+                                        )}
+                                    </div>
+                                </Link>
+
+                                <div className="flex flex-col gap-1">
+                                    <Link href={`/phim/${item.slug}`}
+                                        className="text-xs sm:text-sm text-center hover:text-[#ffd875]">
+                                        <p dangerouslySetInnerHTML={{
+                                            __html: item.name.length > 26
+                                                ? item.name.slice(0, 26) + '...'
+                                                : item.name
+                                        }} />
+                                    </Link>
+
+                                    <Link href={`/phim/${item.slug}`}
+                                        className="text-xs sm:text-sm text-center text-[#AAAAAA]">
+                                        <p dangerouslySetInnerHTML={{
+                                            __html: item.origin_name.length > 27
+                                                ? item.origin_name.slice(0, 27) + '...'
+                                                : item.origin_name
+                                        }} />
+                                    </Link>
+                                </div>
+
+                            </div>
+                        ))}
+                    </div><div className="flex flex-row justify-center items-center my-15">
+                        <button
+                            className={`bg-[#2F3346] rounded-full p-3.5 ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : 'hover:bg-[#3a4054] cursor-pointer'}`}
+                            onClick={() => handlePageChange(currentPage - 1)}
+                            disabled={currentPage === 1 || isLoading}
+                        >
+                            <ArrowLeftIcon className="w-4.5 h-4.5" />
+                        </button>
+
+                        <div className="flex flex-row items-center gap-3 bg-[#2F3346] rounded-full px-5 py-3 mx-2">
+                            <span>Trang</span>
+                            <input
+                                className="w-[50px] h-[28px] bg-[#1C1F2E] rounded text-center"
+                                value={inputPage}
+                                onChange={handleInputChange}
+                                onKeyDown={handleInputSubmit}
+                                disabled={isLoading} />
+                            <span>/ {data?.params.pagination.totalPages || 1}</span>
+                        </div>
+
+                        <button
+                            className={`bg-[#2F3346] rounded-full p-3.5 ${currentPage === (data?.params.pagination.totalPages || 1) ? 'opacity-50 cursor-not-allowed' : 'hover:bg-[#3a4054] cursor-pointer'}`}
+                            onClick={() => handlePageChange(currentPage + 1)}
+                            disabled={currentPage === (data?.params.pagination.totalPages || 1) || isLoading}
+                        >
+                            <ArrowRightIcon className="w-4.5 h-4.5" />
+                        </button>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+}
+
--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -1,42 +1,150 @@
+// @ts-nocheck
 "use client";
 import Form from "next/form";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MagnifyingGlassIcon, XCircleIcon } from '@heroicons/react/20/solid';
+import handleAPIs from "@/lib/api/handleAPI";
+import { MovieDetail } from "@/shared/interfaces/IMovieDetail";
+import { ApiResponse } from "@/shared/interfaces/IApiResponse";
+import Link from "next/link";
+import Loading from "@/components/ui/loading";
+import { escapeHtmlAndEncodeSpaces } from "@/lib/utils";
 
 export default function SearchBar({ isSearchOpen }: { isSearchOpen: boolean }) {
-
     const [query, setQuery] = useState("");
     const [hasQuery, setHasQuery] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+    const [data, setData] = useState<MovieDetail[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-    // Check whether the input contains query
     useEffect(() => {
-        if (query !== "") {
-            setHasQuery(true);
-        } else {
-            setHasQuery(false);
-        }
+        const debounceTimeout = setTimeout(async () => {
+            if (query.trim()) {
+                setHasQuery(true);
+                setIsLoading(true);
+                try {
+                    const response: ApiResponse = await handleAPIs.getData(
+                        `https://phimapi.com/v1/api/tim-kiem?keyword=${escapeHtmlAndEncodeSpaces(query)}&page=1&sort_field=name&sort_type=desc&limit=5`
+                    );
+                    if (response.status) {
+                        setData(response.data.items as MovieDetail[]);
+                    } else {
+                        setData([]);
+                    }
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                    setData([]);
+                } finally {
+                    setIsLoading(false);
+                }
+            } else {
+                setHasQuery(false);
+                setData([]);
+            }
+        }, 300);
+        return () => clearTimeout(debounceTimeout);
+    }, [query]);
 
-    }, [query])         
+    const searchRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (searchRef.current && !searchRef.current.contains(event.target as Node)) {
+                setIsFocused(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    const handleCloseSearch = () => {
+        setQuery("");
+        setHasQuery(false);
+    };
 
     return (
-        <div className={`relative ${isSearchOpen ? 'flex-grow' : 'flex hidden xl:block'} `}>
-            <Form action={"/tim-kiem"}>
+        <div className={`relative ${isSearchOpen ? 'flex-grow' : 'flex hidden xl:block'}`} ref={searchRef}>
+            <Form action={"/tim-kiem"} onSubmit={handleCloseSearch}>
                 <input
                     name="query"
                     value={query}
                     className="peer block w-full rounded-md py-[10px] pl-10 pr-10 text-sm text-white bg-[#FFFFFF14]"
                     placeholder="Tìm kiếm phim"
                     onChange={(e) => setQuery(e.target.value)}
+                    onFocus={() => setIsFocused(true)}
                 />
             </Form>
 
-            <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-white" />
+            <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-white pointer-events-none" />
 
             {hasQuery && (
                 <XCircleIcon
                     className="absolute right-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-white cursor-pointer"
-                    onClick={() => setQuery("")}
+                    onClick={handleCloseSearch}
                 />
+            )}
+
+            {hasQuery && isFocused && (
+
+                <div className="absolute top-full left-0 right-0 mt-1.5 flex flex-col bg-[#15161f] text-white gap-7 p-6 min-w-77 xl:w-97 rounded-t-md">
+                    {isLoading ? (
+                        <div className="flex justify-center">
+                            <Loading width={40} height={40} className={""} />
+                        </div>
+                    ) : data.length > 0 ? (
+                        <>
+                            {data.map((item, index) => (
+                                <Link
+                                    key={index}
+                                    href={`/phim/${item.slug}`}
+                                    className="flex flex-row items-center gap-3.5 hover:opacity-75 cursor-pointer"
+                                >
+                                    <img
+                                        src={`https://phimimg.com/${item.poster_url}`}
+                                        alt={item.name}
+                                        loading="lazy"
+                                        sizes="(max-width: 53px) 100vw"
+                                        className="w-[53px] h-[73px] xs:h-[70px] flex-shrink-0 rounded-sm object-cover">
+                                    </img>
+
+                                    <div className="flex flex-col justify-between gap-1.5 text-[13px] sm:text-sm">
+                                        <p dangerouslySetInnerHTML={{ __html: item.name }} />
+
+                                        <p className="text-[#AAAAAA]"
+                                            dangerouslySetInnerHTML={{
+                                                __html: item.origin_name.length > 40
+                                                    ? item.origin_name.slice(0, 40) + '...'
+                                                    : item.origin_name
+                                            }} />
+
+                                        <div className="flex flex-row items-center text-center text-[#AAAAAA] gap-2">
+                                            <span>{item.year}</span>
+                                            <span>&bull;</span>
+                                            {item.episode_current === "Full" ? (
+                                                <span>{item.time}</span>
+                                            ) : (
+                                                <span>{item.episode_current}</span>
+                                            )}
+                                            <span>&bull;</span>
+                                            <span>{item.lang}</span>
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))}
+
+                            <Link href={`/tim-kiem?query=${query}`}
+                                className="absolute top-full left-0 right-0 flex justify-center text-sm hover:text-[#ffd875] bg-[#25272f] cursor-pointer rounded-b-md py-3.5"
+                                onClick={handleCloseSearch}>
+                                <p>Toàn bộ kết quả</p>
+                            </Link>
+                        </>
+                    ) : (
+                        <p className="text-center text-sm">Không tìm thấy kết quả nào</p>
+                    )}
+                </div>
             )}
         </div>
     );

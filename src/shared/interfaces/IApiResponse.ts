3ccd33a2a143d0602e843fd9c850cf958dddb8cd
Phes <nguyenhongphuc41003@gmail.com>
2025-05-04 00:41:32 +0700

    Add country, episode interface & update properties

--- src/shared/interfaces/IApiResponse.ts
+++ src/shared/interfaces/IApiResponse.ts
@@ -1,12 +1,14 @@
 import { Movie } from "@/shared/interfaces/IMovie";
 import { MovieDetail } from "@/shared/interfaces/IMovieDetail";
 import { Pagination } from "./IPagination";
+import { Episode } from "./IEpisode";
 
 export interface ApiResponse {
     status: boolean | string;
     items: Movie[];
     movie: MovieDetail;
     data: Items;
+    episodes: Episode[];
 }
 
 export interface Items {
--- src/shared/interfaces/IMovieDetail.ts
+++ src/shared/interfaces/IMovieDetail.ts
@@ -1,15 +1,27 @@
 import { Movie } from "./IMovie";
 import { Category } from "./ICategory";
+import { Country } from "./ICountry";
 
 export interface MovieDetail extends Movie {
     content: string;
     type: string;
+    status: string;
     chieurap: boolean;
+    trailer_url: string;
     time: string;
     episode_current: string;
+    episode_total: string;
     quality: string;
     lang: string;
-    actor: any;
+    actor: string[];
+    director: string[];
     category: Category[];
-    country: any;
+    country: Country[];
+    tmdb: {
+        type: string;
+        id: number;
+        season: number;
+        vote_average: number;
+        vote_count: number;
+    }
 }
